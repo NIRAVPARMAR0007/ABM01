23dec6d77b420d6ddfacb0460c1043618d43963e
NIRAVPARMAR0007 <nparmar524@rku.ac.in>
2023-12-26 16:48:22 +0530

    Tue, 26 Dec

---
+++ app/src/main/java/com/example/abm01/HomeFragment.java
@@ -0,0 +1,71 @@
+package com.example.abm01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HomeFragment extends Fragment {
+
+    Button addItemButton, listItemButton;
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        addItemButton = view.findViewById(R.id.addItemButtonId);
+        listItemButton = view.findViewById(R.id.listItemButtonId);
+
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the addItemButton
+                // For example, start an activity to add an item
+                startActivity(new Intent(getActivity(), ItemAdd.class));
+            }
+        });
+
+        listItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click for the listItemButton
+                // For example, start an activity to list items
+                startActivity(new Intent(getActivity(), ItemList.class));
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/abm01/ItemList.java
@@ -0,0 +1,14 @@
+package com.example.abm01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+    }
+}
