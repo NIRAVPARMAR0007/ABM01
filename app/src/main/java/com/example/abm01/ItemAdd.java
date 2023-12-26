647bd46332d2ba2cbe27f963a1da17ab16326fbf
NIRAVPARMAR0007 <nparmar524@rku.ac.in>
2023-12-26 17:42:53 +0530

    Login Page + Navigation Bar + Drawer Navigation + Bottom Sheet Dialong

--- app/src/main/java/com/example/abm01/ItemAdd.java
+++ app/src/main/java/com/example/abm01/ItemAdd.java
@@ -1,14 +1,34 @@
 package com.example.abm01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ItemAdd extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Add item");
+
+        // Enable the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Handle the back button click
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/abm01/ItemList.java
+++ app/src/main/java/com/example/abm01/ItemList.java
@@ -1,14 +1,34 @@
 package com.example.abm01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ItemList extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("List item");
+
+        // Enable the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Handle the back button click
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
